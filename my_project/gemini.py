2c715f3579f31688c0c8ee49671caf2a338775f2
montovoni <montovonii@gmail.com>
2024-09-16 21:49:15 -0300

    #0000 - Api corretas

--- my_project/gemini.py
+++ my_project/gemini.py
@@ -1,6 +1,6 @@
 import google.generativeai as genai
 
-genai.configure(api_key="AIzaSyCTGDxzEQdoOKs2ZpXmrK4aofv-gOO3Q")
+genai.configure(api_key="AIzaSyCTGDxzEQEsdoOKs2ZpXmrK4aofv-gOO3Q")
 model = genai.GenerativeModel('gemini-1.5-flash')
 
 chat_histories = {}
